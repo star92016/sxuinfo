b887faf746db109aad22bae5d2527cb6ce1ff977
licheng <a53133@163.com>
2016-10-02 21:36:14 +0800

    删了旧的的代码

--- app/src/main/java/cn/starnine/sxuinfo/data/MyConfig.java
+++
@@ -1,9 +0,0 @@
-package cn.starnine.sxuinfo.data;
-
-/**
- * Created by licheng on 16-7-29.
- */
-public class MyConfig {
-    public static final String HomeUrl="http://myportal.sxu.edu.cn";
-    public static final String LoginUrl="http://myportal.sxu.edu.cn/userPasswordValidate.portal";
-}
